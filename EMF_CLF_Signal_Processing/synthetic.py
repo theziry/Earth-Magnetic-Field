289c2c4a4045d300c6f359489bb31b05ec104268
thiziriamezza <79851633+thiziriamezza@users.noreply.github.com>
2022-08-09 00:03:02 +0200

    Update synthetic.py

--- EMF_CLF_Signal_Processing/synthetic.py
+++ EMF_CLF_Signal_Processing/synthetic.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-def def_timewindow(t_start, t_end, t_samp):
+def def_timewindow(time_start, time_end, frequency):
     """
     this function returns a time window starting at t_start, finishing at t_end
     with one point every t_samp year
     """
-    mynum = int( (t_end - t_start) / t_samp ) 
-    twindow = np.linspace(t_start, t_end, num=mynum, endpoint = True, dtype=float)
+    mynum = int( (time_end - time_start) / frequency ) 
+    twindow = np.linspace(time_start, time_end, num=mynum, endpoint = True, dtype=float)
     return twindow
 
-def cos_tseries(times, period, amp, phase): 
+def cos_ts(times, period, amp, phase): 
     """
     this function returns amp * cos( 2pi*times/period + phase )
     """
-    tseries = amp * np.cos( 2.*np.pi*times/period + phase )
-    return tseries 
+    ts = amp * np.cos( 2.*np.pi*times/period + phase )
+    return ts 
